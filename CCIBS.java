cfc05401ed3341cc205b69457c042b131158a359
Michael Jonaitis <michaelj@neutroninteractive.com>
2018-02-06 11:26:57 -0700

    re-added all classes in production

---
+++ CCIBS.java
@@ -0,0 +1,14 @@
+public class CCIBS {
+
+
+    public static void main(String[] args) {
+       Enemy enemy_test = new Enemy(20.0, 20.0, 20);
+
+       System.out.println(enemy_test.Dmg());
+       System.out.println(enemy_test.Shoot());
+       
+       Room room_test = new Room();
+       System.out.println(room_test);
+    }
+
+}
