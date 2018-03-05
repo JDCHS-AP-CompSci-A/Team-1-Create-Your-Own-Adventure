37519ea15a7280f559c8e3108b68ed6af5cb7f21
DreadedPyrrha <ryanbarbera@sccstudents.org>
2018-03-05 09:43:30 -0700

    Main and Room Update

--- CCIBS.java
+++ CCIBS.java
@@ -9,14 +9,32 @@ public class CCIBS {
         System.out.println("Welcome to Crash Course in Brain Surgery");
 
         Player player_test = new Player("Jacob", 400.0);
-        System.out.println("You have currently have " + player_test.toString() + " HP");
-        System.out.println("Instructions");
-
-        ActionMenu menu_1 = new ActionMenu();
-        System.out.println("Welcome to the player action menu! Please press 1 to take a shot or 'Q' to quit");
-        menu_1.takeshot_quit();
-        System.out.println(menu_1.toString());
+        System.out.println(player_test.toString());
+        System.out.println();
         
+        ArrayList<Room> roomlist = new ArrayList();
+        Room room_test = new Room("test");
+        Room dungeon = new Room("test");
+        roomlist.add(dungeon);
+        roomlist.add(room_test);
+        
+        while (!roomlist.isEmpty()) {
+            Room current_room = roomlist.get(player_test.position);
+            System.out.println(current_room);
+            System.out.println("Would you like to move forward? (Hit 1 to move forward)");
+            int input = user_input.nextInt();
+            user_input.nextLine();
+            if(input == 1) {
+               roomlist.remove(player_test.position);
+            }
+            
+    }
+
+        
+        
+        
+//        Throne throne_test= new Throne("Throne Room");
+//        System.out.println(throne_test.toStringRoom());
 //        RoomGen gen_test = new RoomGen();
 
 
@@ -52,5 +70,5 @@ public class CCIBS {
 //                System.out.println(room_test.toString());
 //        }
 //    }
-    }
+//    }
 }
--- Room.java
+++ Room.java
@@ -13,7 +13,7 @@ class Room {
         this.getenemy();
     }
 
-    public String toStringRoom() {
+    public String toString() {
         String blank = this.description;
         blank = blank + "\nYou find " + this.enemylist.size() + " enemies \n";
         for (int l =0;l < this.enemylist.size(); l++)
@@ -26,7 +26,7 @@ class Room {
        Enemy Grunt = new Enemy("Grunt", 20.0, 23.0, 10);
        Enemy Brute = new Enemy("Brute", 50.0, 32.0, 20);
        Enemy Elite = new Enemy("Elite", 15.0, 100.0, 0);
-       //Enemy Boss = new Boss("Boss", 240.0, 60.0, 30);
+       
         
         for(int i = 0; i < this.e; i++){
             int type = dice.nextInt(3) + 1;
@@ -37,9 +37,7 @@ class Room {
             }if(type == 3){
                 this.enemylist.add(Elite);
             }
-            //if(this.roomlist.equals(4)){
-            //    this.enemylist.add(Boss);
-            //}
+           
         }
         
     }
